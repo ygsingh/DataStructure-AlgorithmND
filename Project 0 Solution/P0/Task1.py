4887a31d1708974355b839be1094d789bed143f2
Yogendra Singh <ygsingh101@gmail.com>
2020-01-22 21:38:40 -0600

    Upload solution to Task1

--- Project 0 Solution/P0/Task1.py
+++ Project 0 Solution/P0/Task1.py
@@ -14,7 +14,37 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+phoneNumbers = []
+
+for i in range(len(calls)):
+    phoneNumbers.append(calls[i][0])
+    phoneNumbers.append(calls[i][1])
+
+for i in range(len(texts)):
+    phoneNumbers.append(texts[i][0])
+    phoneNumbers.append(texts[i][1])
+
+def uniqueArray(seq):
+   checked = []
+   for e in seq:
+       if e not in checked:
+           checked.append(e)
+   return checked
+
+uniquePhoneNumbers = uniqueArray(phoneNumbers)
+#print(uniquePhoneNumbers)
+count = len(uniquePhoneNumbers)
+
+
+#def test():
+#    print(uniqueArray([1,1,1,1,1,1]))
+#    print(uniqueArray([1,1,2,2,3,3]))
+#test()
+
+print("There are {} different telephone numbers in the records.".format(count))
+#print(len(calls))
+#print(len(texts))
